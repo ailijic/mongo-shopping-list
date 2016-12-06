e9ba9cc6058d7653eb861b578f2a452802b5cdc5
Andrew Ilijic <ailijic@gmail.com>
2016-12-06 15:26:45 -0500

    wrap middleware setup in its own function.

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 start()
 function start () {
   'use strict'
+
+  setupMiddleware()
+}
+
+function setupMiddleware () {
   const express = require('express')
   const bodyParser = require('body-parser')
   const mongoose = require('mongoose')
