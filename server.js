8568a5dcab0db86bbb9649f806b8da46bc0b434a
Andrew Ilijic <ailijic@gmail.com>
2016-12-07 12:23:31 -0500

    Finish 'server.js'

--- server.js
+++ server.js
@@ -1,45 +1,84 @@
+import { hi } from './sweet_code.js'
+hi
+
 start()
 function start () {
   'use strict'
 
-  setupMiddleware()
-}
-
-function setupMiddleware () {
   const express = require('express')
-  const bodyParser = require('body-parser')
-  const mongoose = require('mongoose')
-
-  const config = require('./config')
-
   const app = express()
 
-  app.use(bodyParser.json())
-  app.use(express.static('public'))
+  setupMiddleware()
+  addRoutes()
 
-  function runServer (callback) {
-    mongoose.connect(config.DATABASE_URL, (err) => {
-      if (err && callback) {
-        return callback(err)
-      }
+  function addRoutes () {
+    const Item = require('./models/item')
 
-      app.listen(config.PORT, () => {
-        console.log('Listening on localhost: ', config.PORT)
-        if (callback) {
-          callback()
+    app.get('/items', (req, res) => {
+      Item.find((err, items) => {
+        if (err) {
+          return res.status(500).json({
+            message: 'Internal Server Error'
+          })
         }
+        res.json(items)
+      })
+    })
+
+    app.post('/items', (req, res) => {
+      Item.create({
+        name: req.body.name
+      }, (err, item) => {
+        if (err) {
+          return res.status(500).json({
+            message: 'Internal Server Error'
+          })
+        } else {
+          res.status(201).json(item)
+        }
+      })
+    })
+
+    app.use('*', (req, res) => {
+      res.status(404).json({
+        message: 'Not Found'
       })
     })
   }
 
-  if (require.main === module) {
-    runServer(err => {
-      if (err) {
-        console.error(err)
-      }
-    })
-  }
+  function setupMiddleware () {
+    const bodyParser = require('body-parser')
+    const mongoose = require('mongoose')
 
-  exports.app = app
-  exports.runServer = runServer
+    const config = require('./config')
+
+    app.use(bodyParser.json())
+    app.use(express.static('public'))
+
+    function runServer (callback) {
+      mongoose.connect(config.DATABASE_URL, (err) => {
+        if (err && callback) {
+          return callback(err)
+        }
+
+        app.listen(config.PORT, () => {
+          console.log('Listening on localhost: ', config.PORT)
+          if (callback) {
+            callback()
+          }
+        })
+      })
+    }
+
+    if (require.main === module) {
+      runServer(err => {
+        if (err) {
+          console.error(err)
+        }
+      })
+    }
+
+    exports.app = app
+    exports.runServer = runServer
+  }
 }
