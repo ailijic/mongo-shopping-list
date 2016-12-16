9ba7290ceebe30d5f489f7fa04312966521a0fdd
Andrew Ilijic <ailijic@gmail.com>
2016-12-16 11:14:41 -0500

    Add mongo uname and passwd to config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production'
-                           ? 'mongodb://ailijic:Jonathan87@ds129028.mlab.com:29028/mongoose-shopping-list'
+                           ? 'mongodb://ai:password@ds129028.mlab.com:29028/mongoose-shopping-list'
                            : 'mongodb://localhost/shopping-list-dev')
 exports.PORT = process.env.PORT || 8080
